4997b4f13c2fb01cf2210da1d35e7f23bb36d500
kanyamat <31055915+kanyamat@users.noreply.github.com>
2018-10-25 16:42:21 +0700

    update migrate

--- database/migrations/2017_11_15_015323_users_register.php
+++ database/migrations/2017_11_15_015323_users_register.php
@@ -30,9 +30,6 @@ class UsersRegister extends Migration
             //$table->timestamp('updated_at');
             //$table->rememberToken();
             //$table->timestamps();
-
-
-
             
         });
     }
